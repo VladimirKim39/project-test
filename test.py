48fb5e30794ab92723b945798a3b5bf7e7d8143d
Vladimir Kim <vladimirkimtrading@gmail.com>
2022-06-16 21:46:38 +0200

    drew weird ass circle

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from turtle import color
 import matplotlib.pyplot as plt 
 
 print ('Hello new project')
@@ -10,3 +11,11 @@ z = x+y
 print (z)
 
 print ('this is develop branch')
+
+bigcirc = plt.Circle((0,1), 0.5, color = "blue")
+
+fig,ax = plt.subplots()
+
+
+ax.add_patch(bigcirc)
+fig.savefig("fuck you cunt")
